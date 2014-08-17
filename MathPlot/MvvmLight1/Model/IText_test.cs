5493b0b18cbc3743a814058e2d9cdb542366c161
Tiny_Box@outlook.com <Tiny_Box@outlook.com>
2014-08-18 02:32:01 +0800

    modify

---
+++ MathPlot/MvvmLight1/Model/IText_test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight1.Model
+{
+    interface IText_test
+    {
+        void GetTest(Action<Text_test, Exception> callback);
+    }
+}
---
+++ MathPlot/MvvmLight1/Model/Text_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight1.Model
+{
+    class Text_test
+    {
+        public Text_test(string title)
+        {
+            Title = title;
+        }
+
+        public string Title
+        {
+            get;
+            private set;
+        }
+    }
+}
