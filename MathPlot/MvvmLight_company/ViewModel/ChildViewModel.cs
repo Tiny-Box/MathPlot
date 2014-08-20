2927c4a2a55546a9593002db8f0caa3a1c055bab
TinyBox <Tiny_Box@outlook.com>
2014-08-20 19:58:19 +0800

    modify

---
+++ MathPlot/MvvmLight_company/ViewModel/ChildViewModel.cs
@@ -0,0 +1,103 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace MvvmLight_company.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class ChildViewModel : ViewModelBase
+    {
+
+        #region ICommand
+
+        public RelayCommand LoginCommand
+        {
+            get;
+            private set;
+        }
+
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                return new RelayCommand(
+                    () =>
+                    {
+                        // 发送消息  
+                        Messenger.Default.Send<bool?>(null);
+                    }
+                    );
+            }
+        }  
+        #endregion
+
+        #region close
+        /// <summary>
+        /// 添加Window属性
+        /// </summary>
+        private Window window { get; set; }
+
+
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        public void WindowClose()
+        {
+            this.window.Close();
+        }
+
+        #endregion
+
+        private string _sendStr = "ABC";
+        public string SendStr
+        {
+            get
+            {
+                return _sendStr;
+            }
+            set
+            {
+                if (_sendStr == value)
+                {
+                    return;
+                }
+                _sendStr = value;
+                RaisePropertyChanged("SendStr");
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the ChildViewModel class.
+        /// </summary>
+        public ChildViewModel()
+        {
+
+            #region
+            LoginCommand = new RelayCommand
+            (
+                    () =>
+                    {
+                        //System.Windows.Controls.PasswordBox pb = p as System.Windows.Controls.PasswordBox;
+
+                        //bool isLogon = false;
+                        //string isLogon = "ABC";
+
+                        // 发送消息  
+                        MessageBox.Show(SendStr);
+                        Messenger.Default.Send<string>(SendStr);
+                        
+                    }
+            );
+            #endregion
+        }
+    }
+}
