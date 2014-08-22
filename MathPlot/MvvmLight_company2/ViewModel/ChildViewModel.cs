573db7154e772dee10613b9f7d9f7e62b4e24d31
Tiny_Box@outlook.com <Tiny_Box@outlook.com>
2014-08-23 02:10:45 +0800

    modify-addplot

--- MathPlot/MvvmLight_company2/ViewModel/ChildViewModel.cs
+++ MathPlot/MvvmLight_company2/ViewModel/ChildViewModel.cs
@@ -137,64 +137,8 @@ namespace MvvmLight_company2.ViewModel
         #region Method
         void PlotLine()
         {
-            //
-            // Create the Geometry to draw.
-            //
-            GeometryGroup ellipses = new GeometryGroup();
-            ellipses.Children.Add(
-                new EllipseGeometry(new Point(50, 50), 45, 20)
-                );
-            ellipses.Children.Add(
-                new EllipseGeometry(new Point(50, 50), 20, 45)
-                );
-
-
-            //
-            // Create a GeometryDrawing.
-            //
-            GeometryDrawing aGeometryDrawing = new GeometryDrawing();
-            aGeometryDrawing.Geometry = ellipses;
-
-            // Paint the drawing with a gradient.
-            aGeometryDrawing.Brush =
-                new LinearGradientBrush(
-                    Colors.Blue,
-                    Color.FromRgb(204, 204, 255),
-                    new Point(0, 0),
-                    new Point(1, 1));
-
-            // Outline the drawing with a solid color.
-            aGeometryDrawing.Pen = new Pen(Brushes.Black, 10);
-
-            //
-            // Use a DrawingImage and an Image control
-            // to display the drawing.
-            //
-            DrawingImage geometryImage = new DrawingImage(aGeometryDrawing);
-
-            // Freeze the DrawingImage for performance benefits.
-            geometryImage.Freeze();
-
-            //Image anImage = new Image();
-            anImage.Source = geometryImage;
-            anImage.Stretch = Stretch.None;
-            anImage.HorizontalAlignment = HorizontalAlignment.Left;
-
-            //
-            // Place the image inside a border and
-            // add it to the page.
-            //
-            //Border exampleBorder = new Border();
-            //exampleBorder.Child = anImage;
-            //exampleBorder.BorderBrush = Brushes.Gray;
-            //exampleBorder.BorderThickness = new Thickness(1);
-            //exampleBorder.HorizontalAlignment = HorizontalAlignment.Left;
-            //exampleBorder.VerticalAlignment = VerticalAlignment.Top;
-            //exampleBorder.Margin = new Thickness(10);
-
-            //pan.Margin = new Thickness(20);
-            //pan.Background = Brushes.White;
-            //pan.Content = exampleBorder;
+            StartPoint = new Point(10, 100);
+            EndPoint = new Point(200, 120);
 
         }
         #endregion
